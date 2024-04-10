eaee304870a3a79f9c3caf671dba9e5af8935a57
SFrasia <108404961+SFrasia@users.noreply.github.com>
2024-04-10 09:20:27 +0300

    changes done

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.codition = "New"
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
